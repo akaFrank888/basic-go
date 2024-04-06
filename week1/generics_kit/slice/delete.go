9034a22da283db90b89beee53b44006072db9b82
jike_learning_yxl <1563431499@qq.com>
2024-04-06 16:02:16 +0800

    订正，但未实现扩容

--- week1/generics_kit/slice/delete.go
+++ week1/generics_kit/slice/delete.go
@@ -1,8 +1,16 @@
 package main
 
-func Delete[T any](s []T, idx int) (T, []T) {
+import "fmt"
 
-	res := Find(s, idx)
+func Delete[T any](s []T, idx int) (T, []T, error) {
+
+	if idx < 0 || idx >= len(s) {
+		// 定义一个T的零值
+		var zero T
+		return zero, s, newErrIndexOutOfRange(idx, len(s))
+	}
+
+	deleteValue := s[idx]
 
 	for i := idx; i < len(s)-1; i++ {
 		s[i] = s[i+1]
@@ -11,6 +19,12 @@ func Delete[T any](s []T, idx int) (T, []T) {
 	// 去掉最后一个重复元素 or 删掉index=len-1的元素
 	s = s[:len(s)-1]
 
-	return res, s
+	// TODO 未实现扩容
 
+	return deleteValue, s, nil
+
+}
+
+func newErrIndexOutOfRange(index int, length int) error {
+	return fmt.Errorf("下标超出范围，长度%d，下标%d", length, index)
 }
